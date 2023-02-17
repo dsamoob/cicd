66907e5e4e7a90aba9180da3f477e22ed016420f
dsamoob <dsamoob@yandex.ru>
2023-02-17 20:29:37 +0300

    add test3

--- logistic/test.py
+++ logistic/test.py
@@ -1,7 +1,6 @@
 from rest_framework.test import APITestCase
 
+
 class TestLogistic(APITestCase):
     def test_smth(self):
-        self.assertEqual(10*10, 100)
-
-
+        self.assertEqual(10 * 10, 100)
